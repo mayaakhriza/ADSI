b60d4940de34396020d9fbc453fa17777ba355c0
sisdamalia <37866725+sisdamalia@users.noreply.github.com>
2018-06-04 21:47:58 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Beranda</title>
+		<div class="wrapper2">
+		<center><img src ="images/home.jpg" height="150";/></center>
+		</div>
+		<link rel="stylesheet" type="text/css" href="style.css">
+	</head>
+	<body>
+		<div class="wrapper">
+			<header>	
+				<nav>
+					<ul>
+						<li><a href="index.php"class="current">Home</a></li>
+						<li><a href="produk.php">Produk</a></li>
+						<li><a href="event.php">Event</a></li>
+						<li><a href="infoperusahaan.php">Informasi Perusahaan</a></li>
+						<li><a href="pemesanan.php">Pemesanan</a></li>
+					</ul>
+				</nav>
+			</header>
+			<section class="courses">
+				<article>
+					<figure>
+						<img src="images/eventgeprek2.jpg" height="200"/>
+					</figure>
+					<hgroup>
+						<h2>EVENT BARU!!!</h2>
+						<h3>Yay & Atu suka berMUSIK, NYANYI, atau CIPTAIN LAGU?
+							<br>Kuy Ikutan #MisterGeprekJingleCompetition2018
+							<br>JUTAAN RUPIAH menanti kamu yang mengirimkan karya Terbaik!
+							<br><a href="">Selanjutnya...</a></h3>
+					</hgroup>
+					<p></p>
+				</article> 
+				<article>
+					<figure>
+						<img src="images/pahoman.jpg" height="200"/>
+					</figure>
+					<hgroup>
+						<h2>GRAND OPENING!!!</h2>
+						<h3>#pecintapedas yang tinggal di PAHOMAN mana suaranya?!
+							<br>HARI INI BANGET!!!
+							<br>Mister mau buka cabang baru lhoo di PAHOMAN.
+							<br>Buat kalian yang mau dapet #GeprekGratis
+							<br>bisa dateng ke lokasi yang tertera di gambar ya!
+							<br><a href="">Selanjutnya...</a></h3>
+						</h3>
+					</hgroup>
+					<p></p>
+				</article> 
+			</section>
+			<aside>
+				<section class="popular-recipes">
+					<center>
+					<h2>Kontak Kami</h2>
+					<img src="images/kontak.png" height="100"/>
+					<h3>Email: mistergeprek@yahoo.co.id</h3>
+					<h3>Telepon: 08217887654</h3>
+					</center>
+				</section>
+			</aside>
+			<footer>
+				<center>&copy; 1617051017 - 1617051049 - 1617051075</center>
+			</footer>
+		</div><!-- .wrapper -->
+	</body>
+</html>
